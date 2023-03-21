57c9bd35db0af0c54de0604df977d15f2c6b47ca
RicardoHilknerNeto <rm550757@fiap.com.br>
2023-03-21 11:52:25 -0300

    Adicionando variaveis js

--- rm550757/js/script.js
+++ rm550757/js/script.js
@@ -7,3 +7,17 @@ var h1Element = document.getElementById("titulo-1")
 h1Element.addEventListener("click",function(){
     alert("Agora sou autonômo")    
 })
+
+//Declaração de variaveos com LET, VAR e CONST
+
+var name = "Ricardo" 
+if(nome != ""){
+    var nome = "Neto"
+}
+
+console.log(nome)
+
+
+
+//let name = "Ricardo"
+//const name = "Ricardo"
