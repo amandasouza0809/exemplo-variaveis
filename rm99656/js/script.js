451b36d33a35f5e1eff7486badcba798baf7ce81
joaorihanfiap <rm99656@fiap.com.br>
2023-03-21 11:48:49 -0300

    commitei?

---
+++ rm99656/js/script.js
@@ -0,0 +1,10 @@
+var nome = "tomale"
+var Nome = "ASHAHDAJSODI"
+var nomE = "uau"
+
+var titulo1 = document.getElementById("titulo-1");
+
+titulo1.addEventListener("click", function(){
+    alert("Teste")
+    }
+)
