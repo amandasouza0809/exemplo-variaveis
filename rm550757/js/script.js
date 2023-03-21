b1a346d8649910523fe49159ce9441bfbc3f557d
RicardoHilknerNeto <rm550757@fiap.com.br>
2023-03-21 11:32:23 -0300

    Adicionando EventListener

---
+++ rm550757/js/script.js
@@ -0,0 +1,9 @@
+
+
+//Capturando a tag com ID e colocando em uma variável var
+var h1Element = document.getElementById("titulo-1")
+
+//Adicionando um Listener de Evento
+h1Element.addEventListener("click",function(){
+    alert("Agora sou autonômo")    
+})
