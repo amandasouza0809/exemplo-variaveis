4d85f0959c089ff3ac21aea938af688f8ea5fb18
Amanda Souza <rm97626@fiap.com.br>
2023-03-21 11:55:42 -0300

    exemplo de variaveis

---
+++ rm97626/js/script.js
@@ -0,0 +1,24 @@
+
+
+//Capturando a tag com ID e colocando em uma variavel var
+var h1Elemento = document.getElementById("titulo-1")
+
+//Adicionano um listener de Evento
+h1Elemento.addEventListener("click", function(){
+
+    alert ("Agora sou autonômo!")
+
+})
+
+//Declaração de variaveis  com LET e VAR e CONST
+
+//const nome = "Amanda"
+
+var nome = "Amanda"
+
+if(nome != ""){
+    var nome  = "Emanuelle"
+}
+
+//Imprimindo o valor da variavel
+console.log (nome)
