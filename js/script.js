a94a136c2a3e02eb2edcb058e33930978e3790ab
MatheusCosta616 <rm551157@fiap.com.br>
2023-03-21 11:47:08 -0300

    JS ATUALIZDO

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//capturando o ID e colocando em uma var
+
+let h1Elemento = document.getElementById("titulo-1")
+
+h1Elemento = addEventListener("click", function(){
+    alert("Agora sou autônomo")
+})
+
+var nome = "Pedro"
+let nome = "Pedro"
+const nome = "Pedro"
