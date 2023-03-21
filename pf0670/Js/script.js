9bd09c59c742fd4fe70261cb6cf7e713a7ef1e79
GabrielMachadoCP <rm99880@fiap.com.br>
2023-03-21 11:32:15 -0300

    Adicionamos um listener de evento

---
+++ pf0670/Js/script.js
@@ -0,0 +1,11 @@
+
+
+//Capturando a tag com o ID e colocando em uma variável var
+var h1Elemento = document.getElementById("titulo-1")
+
+//Adicionando um listener de evento
+h1Elemento.addEventListener("click", function(){
+
+alert("Agora sou autônomo!")
+
+})
