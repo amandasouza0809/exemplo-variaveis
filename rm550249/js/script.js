08860ab0d823276e9466aa29187cfe0efee718bb
RodriguesEric134 <rm550249@fiap.com.br>
2023-03-21 11:56:20 -0300

    Exemplo de utilização do JS + obj document

---
+++ rm550249/js/script.js
@@ -0,0 +1,18 @@
+//capturando a tag com id e colocando em uma variável var
+var h1Elemento = document.getElementById("titulo-1")
+
+//Adicionando um listener de Evento
+h1Elemento.addEventListener("click", function(){
+
+alert("agora sou autonômo")
+
+})
+
+//Delcaração de variáveis com LET, VAR e CONST
+
+var nome = "Eric"
+if (nome != ""){
+    let nome = "Rodrigues"
+} 
+
+console.log(nome)
