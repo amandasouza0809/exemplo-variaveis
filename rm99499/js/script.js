42a3f79c82310e40d6faabf152851925a94cec8a
GuiEspimFiap <rm99499@fiap.com.br>
2023-03-21 11:40:55 -0300

    html and js

---
+++ rm99499/js/script.js
@@ -0,0 +1,7 @@
+// capiturando a tag por id e colocando em uma variável var
+
+var h1Element = document.getElementById("title-1"); 
+
+h1Element.addEventListener("click", function() {
+    alert("Agora sou autonomo!")
+});
