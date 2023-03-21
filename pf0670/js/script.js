fe33ba705d204c521e47007d664bc5c22a1b8831
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-03-21 11:57:54 -0300

    Criado aruivo js, evento + declarações.

---
+++ pf0670/js/script.js
@@ -0,0 +1,24 @@
+
+
+//Capturando a tag com ID e colocando em uma variável var
+var h1Elemento = document.getElementById("titulo-1")
+
+//Adicionando um Listener de Evento
+h1Elemento.addEventListener("click",function(){
+
+    alert("Agora sou autonômo!")
+
+})
+
+//Declaração de variáveis com LET e VAR e CONST
+
+//const nome = "Pedro"
+
+var nome = "Pedro"
+
+if(nome != ""){
+    let nome = "Emanuelle"
+}
+
+//Imprimindo o valor da variável
+console.log(nome)
