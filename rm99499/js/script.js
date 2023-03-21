05089b360b425fce9e992a3f8a8da6934591c325
GuiEspimFiap <rm99499@fiap.com.br>
2023-03-21 11:54:33 -0300

    javascript com var, let e const

--- rm99499/js/script.js
+++ rm99499/js/script.js
@@ -3,5 +3,17 @@
 var h1Element = document.getElementById("title-1"); 
 
 h1Element.addEventListener("click", function() {
-    alert("Agora sou autonomo!")
+    alert("Agora sou autonomo!");
 });
+
+
+// declaração de variáveis de let, var e const
+
+var nome = "Guilherme";
+
+if (nome !== "") {
+    let nome = "Heloísa";
+}
+console.log(nome)
+
+// const nome = "Guilherme"; 
